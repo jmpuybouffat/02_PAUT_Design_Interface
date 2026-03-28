5b04fad1574f67a6b66004f54041624cbba114fe
Jean michel Puybouffat <58659149+jmpuybouffat@users.noreply.github.com>
2026-03-28 16:49:56 +0100

    Update extrados_wedge_pa.py

--- scripts/extrados_wedge_pa.py
+++ scripts/extrados_wedge_pa.py
@@ -2,14 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-t = np.linspace(-30, 110, 1000)
+# Extrados
+def extrados(x):
+    return 0.001808914648*x**2 - 0.1314763917*x + 17.33694558
 
-x = t
-y = -0.003609426882*t**2 + 0.3901521821*t + 19.70318543
-z = -0.005488853532*t**2 + 0.4063539903*t + 322.5637160
+x = np.linspace(-35, 115, 100)
+y_extrados = extrados(x)
+
+# Indications extrados
+def indication_extrados(x):
+    return -0.002120007284*x**2 + 0.01039951273*x + 275.5911374
+
+x_ind = np.linspace(-34, 113, 100)
+z_ind = indication_extrados(x_ind)
+y_ind = np.zeros_like(x_ind)  # y = 0
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(x, y, z, label='Courbe 3D', color='blue')
+ax.plot(x, np.zeros_like(x), y_extrados, label='Extrados')
+ax.plot(x_ind, y_ind, z_ind, label='Indications Extrados', color='red')
 ax.view_init(elev=20, azim=-60)
 plt.show()
