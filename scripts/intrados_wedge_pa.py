2e727ba1cfbb8e3e1862059607305e6a6e3b455d
Jean michel Puybouffat <58659149+jmpuybouffat@users.noreply.github.com>
2026-03-28 16:42:35 +0100

    Update intrados_wedge_pa.py

--- scripts/intrados_wedge_pa.py
+++ scripts/intrados_wedge_pa.py
@@ -2,16 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Surface intrados (mm)
-def intrados(x):
-    return -0.001808914648*x**2 + 0.1314763917*x - 17.33694558
+t = np.linspace(-50, 150, 1000)
 
-# PA + wedge 55°
-x = np.linspace(-35, 115, 100)
-y = intrados(x)
+x = t
+y = 0.001808914648*t**2 - 0.1314763917*t + 17.33694558
+z = -0.003862179289*t**2 + 0.1391544251*t + 276.4370567
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(x, np.zeros_like(x), y, label='Intrados')
+ax.plot(x, y, z, label='Courbe 3D', color='blue')
 ax.view_init(elev=20, azim=-60)
 plt.show()
