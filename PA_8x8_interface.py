0ea5b7859b77aa459be04e1c7c93e3f738fac5c5
Jean michel Puybouffat <58659149+jmpuybouffat@users.noreply.github.com>
2026-03-31 14:37:00 +0200

    Update PA_8x8_interface.py

--- PA_8x8_interface.py
+++ PA_8x8_interface.py
@@ -1,5 +1,13 @@
-t = float(input("Entrez la position t sur la courbe (-30 à 110) : "))
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(-30, 110, 100)
 x = t
 y = -0.003609404789*t**2 + 0.3901494991*t + 19.70324623
 z = -0.005488828669*t**2 + 0.4063509956*t + 322.5637799
-print(f"Position PA : x={x:.2f}, y={y:.2f}, z={z:.2f}")
+
+plt.plot(t, y, label='Y(t)')
+plt.plot(t, z, label='Z(t)')
+plt.xlabel('t')
+plt.legend()
+plt.show()
